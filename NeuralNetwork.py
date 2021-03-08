38e3ef47b05b935193c556e8016967b29cb48fa9
Jake Massoth <jake@massoth.tech>
2021-03-08 23:03:41 +0100

    implemented simple forward pass and prediction, no backprop or training yet

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,19 +1,57 @@
 import numpy as np
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
 class NeuralNetwork:
-    def __init__(self, input_layer, output_layer, num_hidden_layers, nodes_per_hidden_layer):
-        # These must be numpy arrays
-        self.input_layer = input_layer
-        self.output_layer = output_layer
+    def __init__(self, num_input_nodes, num_output_nodes, num_hidden_layers, nodes_per_hidden_layer):
+        assert num_hidden_layers > 0, "Number of hidden layers must be more than 0."
+
+        self.num_input_nodes = num_input_nodes
+        self.num_output_nodes = num_output_nodes
         self.num_hidden_layers = num_hidden_layers
         self.nodes_per_hidden_layer = nodes_per_hidden_layer
-        self.weights = np.array([np.random.rand(self.input_layer.shape[1], self.nodes_per_hidden_layer),
-                                 np.full(num_hidden_layers, np.random.rand(self.nodes_per_hidden_layer,
-                                                                           self.nodes_per_hidden_layer)),
-                                 np.random.rand(nodes_per_hidden_layer, output_layer.shape)])
-        self.output_layer = np.zeros(output_layer.shape)
+        self.output_layer = np.zeros(num_output_nodes)
+        self.input_layer = np.zeros(num_input_nodes)
+        self.is_trained = True
+        self.weights = []
+        self.biases = []
+
+        # set up weights: starting with the weights from the input layer to the first hidden layer
+        self.weights.append(np.random.randn(self.nodes_per_hidden_layer, self.num_input_nodes))
+        if self.num_hidden_layers > 1:
+            for _ in range(self.num_hidden_layers):
+                self.weights.append(np.random.randn(self.nodes_per_hidden_layer,
+                                                    self.nodes_per_hidden_layer))
+        # weights from the last hidden layer to the output layer
+        self.weights.append(np.random.randn(num_output_nodes, nodes_per_hidden_layer))
+
+        # set up biases
+        self.biases.append(np.random.randn(nodes_per_hidden_layer))
+        if self.num_hidden_layers > 1:
+            for _ in range(self.num_hidden_layers):
+                self.biases.append(np.random.randn(self.nodes_per_hidden_layer))
+        self.biases.append(np.random.randn(num_output_nodes))
+
+    def train(self, learning_rate, examples):
+        pass
+
+    def __backward_pass(self):
+        pass
+
+    def __forward_pass(self, x):
+        a = [np.copy(x)]
+        z = []
+        for i in range(len(self.weights)):
+            z.append(self.weights[i].dot(a[-1]) + self.biases[i])
+            a.append(self.__sigmoid(z[-1]))
+        return z, a
+
+    def __back_prop(self, x):
+        pass
+
+    def __sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def predict(self, x):
+        assert self.is_trained, "Model is not trained. Please provide some input examples and run the train() function"
+        _, y = self.__forward_pass(x)
+        return y[-1]
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import NeuralNetwork as nn
+from NeuralNetwork import NeuralNetwork
 import numpy as np
 
 if __name__ == '__main__':
-    print("lets do some neural networks my dudes")
+    nn = NeuralNetwork(9, 9, 100, 10)
+    y = nn.predict([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    print(y)
