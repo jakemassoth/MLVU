9e81789388b513b8181af6699440ec575f17a344
leo <leander.finck@gmail.com>
2021-03-16 18:18:46 +0100

    added the keras neural network

---
+++ StateOfTheArt.py
@@ -0,0 +1,32 @@
+from test import prepare_data
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import Adam
+from keras.callbacks import History
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    (x_train, y_train), (x_test, y_test) = prepare_data()
+
+    hist = History()
+    EPOCH = 20
+    LEARNING_RATE = 0.01
+
+    model = Sequential()
+    model.add(Dense(128, input_dim=784, activation='sigmoid'))
+    model.add(Dense(10, activation='sigmoid')) #or, activation='softmax'
+
+    optimizer = Adam(lr=LEARNING_RATE)  # Adam uses stochastic gradient descent
+    model.compile(optimizer=optimizer, loss='MSE', metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=EPOCH, callbacks=[hist])
+
+    y_loss = hist.history['loss']
+    x_epoch = np.arange(0, EPOCH, 1)
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss value')
+    plt.scatter(x_epoch, y_loss)
+    plt.show()
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+import NeuralNetwork as nn
+import numpy as np
+
+if __name__ == '__main__':
+    print("lets do some neural networks my dudes")
